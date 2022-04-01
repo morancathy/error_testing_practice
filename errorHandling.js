58ff67beb73556b47b09c99caf41579d55d9ee42
Cat <morancathy@gmail.com>
2022-03-31 21:17:23 -0500

    test for array arguement

--- errorHandling.js
+++ errorHandling.js
@@ -1,11 +1,9 @@
 //////// Check for 2 arguements and integers
 function maxOfTwoNumbers(...nums) {
   let max = 0;
-
   if (nums.length !== 2) {
     throw new Error("input exactly two nums");
   }
-
   nums.forEach((num) => {
     if (typeof num !== "number") {
       throw new Error("input two integers");
@@ -13,10 +11,8 @@ function maxOfTwoNumbers(...nums) {
       max = num;
     }
   });
-
   return max;
 }
-
 console.log("max of two numbers: ", maxOfTwoNumbers(14, 7));
 
 //////// check for one character
@@ -26,5 +22,14 @@ function isCharVowel(char) {
   }
   return "aeiou".includes(char.toLowerCase());
 }
-
 console.log("Is this a vowel: ", isCharVowel("E"));
+
+///// check for array
+const sumArray = (nums) => {
+  if (!Array.isArray(nums)) {
+    throw new Error("argument must be an array");
+  } else {
+    return nums.reduce((acc, elem) => acc + elem, 0);
+  }
+};
+console.log("sum of the array: ", sumArray([1, 2, 3]));
